90c96bfe3f6dd51853bc43336cc4ba1ceefcf1a8
Miles Pratt <miles@thoughtpowder.com>
2020-02-15 15:42:13 -0500

    update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Select profile photo text", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Select profile photo/i);
   expect(linkElement).toBeInTheDocument();
 });
